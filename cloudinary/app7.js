9a4c5d628c0d88b01db8e2ffb3849ecdd4200e65
Shashankh09 <153710936+Shashankh09@users.noreply.github.com>
2024-01-24 18:55:38 +0530

    Add files via upload

---
+++ cloudinary/app7.js
@@ -0,0 +1,40 @@
+const cloudName = 'dwatvo2cs';
+const uploadPreset = 'Eduhelpforum';
+const cloudinaryFolder = 'Chemistry';
+
+
+const myWidget = cloudinary.createUploadWidget({
+    cloudName: cloudName,
+    uploadPreset: uploadPreset,
+    folder: cloudinaryFolder, 
+    resourceType: 'image',   
+}, (error, result) => {
+    if (!error && result && result.event === "success") {
+        const imageUrl = result.info.secure_url;
+
+        
+        firebase.database().ref('images').push({
+            imageUrl,
+        });
+    }
+});
+
+
+myWidget.open();
+
+
+document.getElementById('cloudinary-upload-widget').addEventListener('click', () => {
+    myWidget.open();
+});
+
+
+firebase.database().ref('images').on('child_added', (snapshot) => {
+    const imageUrl = snapshot.val().imageUrl;
+
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    imgElement.style.marginRight='10px';
+    imgElement.style.width='800px';
+    imgElement.style.width='500px';
+    document.getElementById('imageContainer').appendChild(imgElement);
+});
---
+++ cloudinary/config2.js
@@ -0,0 +1,19 @@
+// Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyBHOz1UEIwWKTsKzMTYRs1_F2lUaiaNovY",
+  authDomain: "english-bd9ff.firebaseapp.com",
+  projectId: "english-bd9ff",
+  storageBucket: "english-bd9ff.appspot.com",
+  messagingSenderId: "289812100428",
+  appId: "1:289812100428:web:4949f0d0445c2a3d320be9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Cloudinary config
+cloudinary.config({
+    cloud_name: 'dwatvo2cs',
+    api_key: '895648459221571',
+    api_secret: 'N-4XC21J19pNOna46oeBXVcj34M'
+});
